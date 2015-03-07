dac8e408b3e4b1f05a031c658fc83731a5f50c72
cash <dad.09.michael@gmail.com>
2015-03-07 20:15:37 +0100

    bugfixing

--- back.php
+++ back.php
@@ -9,7 +9,7 @@ include('header.php');
             <div class="blogtitle"><h3>Letzte Veranstaltunge</h3></div>
             <div class="blog">
                 <div class="one-third">
-                    <img src="img/cimfid.jpeg" alt=""/>
+                    <img src="img/cimfid-house.jpeg" alt=""/>
                 </div>
                 <div class="two-third">
                     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
