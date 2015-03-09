1f3b40539a59f4720f1f237f5786064e00324a56
cash <dad.09.michael@gmail.com>
2015-03-09 23:27:54 +0100

    modified config.php

--- src/config.php
+++ src/config.php
@@ -1,9 +1,18 @@
 <?php
-// db-host
-define('DB_HOST', 'localhost');
-define('DB_NAME', 'vacc_db');
-define('DB_USER', 'dbviewer');
-define('DB_PW', '0ItZ58ucz3ckXDPOWes9');
+
+if ($_SERVER["SERVER_NAME"] == 'localhost') {
+    // local db
+    define('DB_HOST', 'localhost');
+    define('DB_NAME', 'vacc_db');
+    define('DB_USER', 'dbviewer');
+    define('DB_PW', '0ItZ58ucz3ckXDPOWes9');
+} else {
+    // 1&1 db
+    define('DB_HOST', 'db568509021.db.1and1.com');
+    define('DB_NAME', 'db568509021');
+    define('DB_USER', 'dbo568509021');
+    define('DB_PW', '0ItZ58ucz3ckXDPOWes9');
+};
 
 // connect to db
 $dbh = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PW);
